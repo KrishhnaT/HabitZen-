30292ed2b83af3d09370c6e9efc613bc6e68804a
Pranav Kunjir <pranavkunjir93@gmail.com>
2024-10-08 21:40:43 +0530

    added mongo db works on my machine took longer than expected

--- app.js
+++ app.js
@@ -3,16 +3,33 @@ var express = require('express');
 var path = require('path');
 var fs = require("fs");
 var url = require("url");
+var mongoose = require("mongoose")
+// import mongoose from "mongoose";
 
 var app = express();
 app.use(express.static(path.join(__dirname + "/public")));
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
-
+mongoose.connect("mongodb://localhost:27017/database").then(() =>{
+    console.log("db connected")
+})
 const myServer = http.createServer(app);
 
 myServer.listen(8000,()=> console.log('server started'))
+
+const userSchema = new mongoose.Schema({
+    name: String,
+    age: Number,
+});
+
+const UserModel = mongoose.model("users", userSchema)
+
+app.get("/getUsers", async(req,res) =>{
+    const userData = await UserModel.find();
+    res.json(userData)
+});
+
 
 
 module.exports = app;
